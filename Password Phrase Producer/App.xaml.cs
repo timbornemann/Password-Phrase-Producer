6371468a1a1b4ac378cd38222e069d003b1e7868
Tim Bornemann <timbornemann@users.noreply.github.com>
2024-10-04 18:56:19 +0200

    Create maui project

---
+++ Password Phrase Producer/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Password_Phrase_Producer
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Password Phrase Producer/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Password_Phrase_Producer
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
