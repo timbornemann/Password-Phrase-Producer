6fbed58ddef3556eed7b11cbec9c98c4f6fca22c
Tim Bornemann <timbornemann@users.noreply.github.com>
2024-10-05 23:42:45 +0200

    Add interface for TBV's

---
+++ Password Phrase Producer/PasswordGenerationTechniques/TbvTechniques/Itbv.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Password_Phrase_Producer.PasswordGenerationTechniques.TbvTechniques
+{
+    public interface Itbv
+    {
+       string GeneratePassword(string plainTextPassword, int code1, int code2, int code3);
+    }
+}
