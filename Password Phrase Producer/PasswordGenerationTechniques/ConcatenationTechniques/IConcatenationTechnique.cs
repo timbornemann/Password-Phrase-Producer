d78fb434c17114e75ea5c58c16d41b10fe44ff96
Tim Bornemann <timbornemann@users.noreply.github.com>
2024-10-06 15:23:04 +0200

    Add Concatination techniqe interface

---
+++ Password Phrase Producer/PasswordGenerationTechniques/ConcatenationTechniques/IConcatenationTechnique.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Password_Phrase_Producer.PasswordGenerationTechniques.ConcatenationTechniques
+{
+    public interface IConcatenationTechnique
+    {
+        string EncryptPassword(IEnumerable<string> plainTextWords);
+    }
+}
