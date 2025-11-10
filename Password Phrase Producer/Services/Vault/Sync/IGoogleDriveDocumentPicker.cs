fb3b0a65a109361c16f798b8e3b149301a30031d
Tim Bornemann <65897899+timbornemann@users.noreply.github.com>
2025-11-10 12:28:42 +0100

    Use Android SAF for Google Drive sync

---
+++ Password Phrase Producer/Services/Vault/Sync/IGoogleDriveDocumentPicker.cs
@@ -0,0 +1,11 @@
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Password_Phrase_Producer.Services.Vault.Sync;
+
+public interface IGoogleDriveDocumentPicker
+{
+    Task<string?> CreateDocumentAsync(string suggestedFileName, CancellationToken cancellationToken = default);
+
+    void ReleasePersistedPermission(string documentUri);
+}
